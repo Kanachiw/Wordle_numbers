c2a9a3a72004c3b062f73c7d7f4fc474686a5772
Kanachiw <weli.k@northeastern.edu>
2023-01-30 17:57:22 -0500

    finish correcting error

--- part_a.cpp
+++ part_a.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <iomanip> 
 #include <stdexcept>
+#include <iostream>
+#include <algorithm>
 //#ifndef LOCKBOX_CLASS
 //#define LOCKBOX_CLASS
 
@@ -11,86 +13,127 @@ class LockBox {
 
   private:
 
-    vector<int> code;
+    vector<int> lockcode;
+  
     int length, digits; 
 
   public:
 
-    LockBox(int n, int m);
+    LockBox(int n=0, int m=0);
 
-    void LockCode(int n, int m); 
+    int getLength();
+
+    int getDigits(); 
+
+    void LockCode(); 
 
     void guess();
 
-    LockBox correctLocation(const LockBox &guess);
+    int  correctLocation(const LockBox &guess);
 
-    int incorrectLocation(const LockBox &guess);
+    int incorrectLocation( const LockBox &guess);
 
     int getCode () const;
 };
 
 LockBox :: LockBox(int n, int m) : length(n), digits(m) // allow the 
 {
-
-  LockCode(n, m);
-  
+  length= n;
+  digits = m;
 }
 
-void LockBox :: LockCode(int n, int m) {
+int LockBox ::  getLength()
+{
+  return length;
+
+}
+
+int LockBox :: getDigits()
+{
+ return digits; 
+}
+
+//create a lockode 
+void LockBox :: LockCode() {
   
-    vector<int> code; //initializes a vector of integer 
+     //initializes a vector of integer 
   
-    for (int i = 0; i < n; i++){ // create a loop to create a vector size of n 
-  
-      code.push_back(rand() % m-1); // generate each individal digit from the range of [0,m-1]
-      
+    for (int i = 0; i < length; i++){ // create a loop to create a vector size of n 
+      int item = rand() % (digits-1);
+      lockcode.push_back(item); // generate each individal digit from the range of [0,m-1]
+      cout<<item<<endl;
     }
 }
 
 
-void LockBox :: guess() {
+void LockBox :: guess() { 
 //input guess and store as vector
-  vector<int> guess; 
+  
   int num; 
   
      for (int i = 0; i < length; i++){ // create a loop to create a vector size of n 
-      cin>> num; 
+      cout<<"enter digit "<<i<<" from 0: [" <<length<<"-1]"<<endl;;
+      cin >> num; 
       if (num > (digits-1)){
-        throw"range error"; 
+        throw "range error"; 
       }
-      guess.push_back(num); // generate each individal digit from the range of [0,m-1]
+      lockcode.push_back(num); // generate each individal digit from the range of [0,m-1]
       
     }
+
+    
 }
 
 
-LockBox::LockBox correctLocation(const LockBox &guess) {
 
-  count1 = // number of correct digits 
+int LockBox :: correctLocation(const LockBox &guess) {
 
-  for (int i = 0; i < n; i++) {
-    if (guess != code) {
-      
+  int c1= 0;// couts the  number of correct digits 
+
+  for (int i = 0; i < length; i++) {
+
+    if (lockcode[i] ==guess.lockcode[i]){
+      c1++; 
     }
-    return count1;
+    
   }
+  return c1; 
 }
+int LockBox:: incorrectLocation( const LockBox &guess){
+int c2=0;//number of correct digits in wrong location
 
-LockBox::LockBox incorrectLocation(const LockBox &guess) {
+ //create a vecotor of unique guesses 
+  vector<int> temp =guess.lockcode;
+  vector <int> uniqueGuess; 
 
-  count2 = //number of correct digits in wrong location
+  for (int i = 0; i < length; i++) {
 
-  for (int i = 0, i < n; i++) {
-    if (guess != code) {
-      
+    if (find(uniqueGuess.begin(),uniqueGuess.end(), temp[i])!= uniqueGuess.end()) {
+      uniqueGuess.push_back(temp[i]); 
     }
-    return count2; 
-  } 
+  
+  }
+
+  for(int i=0; i<uniqueGuess.size(); i++){
+
+    if (find(lockcode.begin(),lockcode.end(), uniqueGuess[i])!= lockcode.end()) {
+      c2++; 
+    }
+  }
+  
+  return c2; 
+
 }
 
 
 int main () {
-
-
+LockBox obj(3,7);
+int length = obj.getLength();
+int digits = obj.getDigits();
+obj.LockCode();
+LockBox obj2(length, digits); 
+obj2.guess();
+cout << obj.correctLocation(obj2);
+cout<< obj.incorrectLocation(obj2);
   
 }
