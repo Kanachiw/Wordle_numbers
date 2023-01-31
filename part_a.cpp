551a0944b6db9dc33cde00f1ab5446187ccf7c5b
Kanachiw <weli.k@northeastern.edu>
2023-01-31 16:28:57 -0500

    part a working

--- part_a.cpp
+++ part_a.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 #include <iostream>
 #include <algorithm>
+#include <cstdlib> 
 //#ifndef LOCKBOX_CLASS
 //#define LOCKBOX_CLASS
 
@@ -33,7 +34,7 @@ class LockBox {
 
     int incorrectLocation( const LockBox &guess);
 
-    int getCode () const;
+    //int getCode () const;
 };
 
 LockBox :: LockBox(int n, int m) : length(n), digits(m) // allow the 
@@ -57,9 +58,9 @@ int LockBox :: getDigits()
 void LockBox :: LockCode() {
   
      //initializes a vector of integer 
-  
+    srand(time(0));
     for (int i = 0; i < length; i++){ // create a loop to create a vector size of n 
-      int item = rand() % (digits-1);
+      int item = rand() % digits;
       lockcode.push_back(item); // generate each individal digit from the range of [0,m-1]
       cout<<item<<endl;
     }
@@ -92,7 +93,7 @@ int LockBox :: correctLocation(const LockBox &guess) {
 
   for (int i = 0; i < length; i++) {
 
-    if (lockcode[i] ==guess.lockcode[i]){
+    if (lockcode[i] == guess.lockcode[i]){
       c1++; 
     }
     
@@ -103,37 +104,34 @@ int LockBox:: incorrectLocation( const LockBox &guess){
 int c2=0;//number of correct digits in wrong location
 
  //create a vecotor of unique guesses 
-  vector<int> temp =guess.lockcode;
-  vector <int> uniqueGuess; 
-
-  for (int i = 0; i < length; i++) {
-
-    if (find(uniqueGuess.begin(),uniqueGuess.end(), temp[i])!= uniqueGuess.end()) {
-      uniqueGuess.push_back(temp[i]); 
-    }
+  vector <int> unique; 
   
-  }
 
-  for(int i=0; i<uniqueGuess.size(); i++){
-
-    if (find(lockcode.begin(),lockcode.end(), uniqueGuess[i])!= lockcode.end()) {
-      c2++; 
+   for (int i = 0; i < length; i++) {
+   
+    if ((count (unique.begin(), unique.end(), guess.lockcode[i]))==0 
+    && (count (lockcode.begin(), lockcode.end(), guess.lockcode[i]))>0 
+    &&lockcode[i]!= guess.lockcode[i] )
+    { 
+       unique.push_back(guess.lockcode[i]); 
+        c2++; 
+        
     }
-  }
-  
+    
+   }
+
   return c2; 
-
 }
 
 
 int main () {
-LockBox obj(3,7);
+LockBox  obj(4,5);
 int length = obj.getLength();
 int digits = obj.getDigits();
 obj.LockCode();
 LockBox obj2(length, digits); 
 obj2.guess();
-cout << obj.correctLocation(obj2);
-cout<< obj.incorrectLocation(obj2);
+cout <<"correct location " << obj.correctLocation(obj2)<<endl;
+cout<<"incorrect location "<< obj.incorrectLocation(obj2)<<endl;
   
 }
